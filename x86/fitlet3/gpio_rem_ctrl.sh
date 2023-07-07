9a9695aa163ad579ec06708fdb419c091a08b34c
vraevsky <valentin@compulab.co.il>
2023-07-07 12:29:25 +0300

    Update gpio_rem_ctrl.sh

--- x86/fitlet3/gpio_rem_ctrl.sh
+++ x86/fitlet3/gpio_rem_ctrl.sh
@@ -2,5 +2,5 @@
 
 PCA9555=0x20
 
-sm_bus=$(i2cdetect -l | awk '(/smbus/)&&($0=$1)')
-echo ${PCA9555} > /sys/bus/i2c/devices/${sm_bus}/delete_device
+smbus=$(i2cdetect -l | awk '(/smbus/)&&($0=$1)')
+echo ${PCA9555} > /sys/bus/i2c/devices/${smbus}/delete_device
