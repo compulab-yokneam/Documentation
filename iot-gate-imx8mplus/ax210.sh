99494b5e66049e63e3489aa8ac9461fb5d16f6c9
vraevsky <valentin@compulab.co.il>
2024-05-21 17:39:49 +0300

    Update ax210.sh

--- iot-gate-imx8mplus/ax210.sh
+++ iot-gate-imx8mplus/ax210.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-for f in iwlwifi-ty-a0-gf-a0-62.ucode iwlwifi-ty-a0-gf-a0-63.ucode iwlwifi-ty-a0-gf-a0-66.ucode iwlwifi-ty-a0-gf-a0-67.ucode;
+for f in iwlwifi-ty-a0-gf-a0-62.ucode iwlwifi-ty-a0-gf-a0-63.ucode iwlwifi-ty-a0-gf-a0-66.ucode iwlwifi-ty-a0-gf-a0-67.ucode iwlwifi-ty-a0-gf-a0.pnvm;
 do
   rm -rf /lib/firmware/$f;
 done
