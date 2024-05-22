772cf98c205786beb4d04ff4bbb70604dbb32f35
vraevsky <valentin@compulab.co.il>
2024-05-22 16:39:29 +0300

    Update ax210.sh

--- iot-gate-imx8mplus/ax210.sh
+++ iot-gate-imx8mplus/ax210.sh
@@ -1,6 +1,31 @@
 #!/bin/bash -x
 
-for f in iwlwifi-ty-a0-gf-a0-62.ucode iwlwifi-ty-a0-gf-a0-63.ucode iwlwifi-ty-a0-gf-a0-66.ucode iwlwifi-ty-a0-gf-a0-67.ucode iwlwifi-ty-a0-gf-a0.pnvm;
-do
-  rm -rf /lib/firmware/$f;
-done
+ACTIVE_DIR=/lib/firmware
+HIDDEN_DIR=/lib/firmware/hidden
+
+FIRMWARE_FILE=" \
+        iwlwifi-ty-a0-gf-a0-62.ucode \
+        iwlwifi-ty-a0-gf-a0-63.ucode \
+        iwlwifi-ty-a0-gf-a0-66.ucode \
+        iwlwifi-ty-a0-gf-a0-67.ucode \
+        iwlwifi-ty-a0-gf-a0.pnvm \
+"
+
+_move_file() {
+        [[ -f "${file:-/x/y/z}" ]] || return 0
+        mkdir ${dest} -p
+        mv ${file} ${dest}/
+}
+
+move_files() {
+        for f in ${FIRMWARE_FILE};
+        do
+          [[ -f ${source_dir}/$f ]] && file=${source_dir}/$f dest=${destination_dir} _move_file
+        done
+}
+
+DIRRECTION=${DIRRECTION:-0}
+
+[[ ${DIRRECTION} -eq 0 ]] && source_dir=${ACTIVE_DIR} destination_dir=${HIDDEN_DIR} || source_dir=${HIDDEN_DIR} destination_dir=${ACTIVE_DIR}
+
+source_dir=${source_dir} destination_dir=${destination_dir} move_files
