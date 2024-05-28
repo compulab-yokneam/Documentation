742ee8c7cf02e33f4461e7ef2c3e99a1eed2d977
vraevsky <valentin@compulab.co.il>
2024-05-28 16:39:46 +0300

    Create docker.sh

---
+++ ucm-imx8m-plus/docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+function get_n_install() {
+
+    docker+="https://github.com/compulab-yokneam/bin/raw/docker/docker/docker-moby-cli_24.0.5+git00e46f85f6e46bb4b02c33da253f901c473794e9-r0_arm64.deb "
+    docker+="https://github.com/compulab-yokneam/bin/raw/docker/docker/docker-moby_24.0.5+git00e46f85f6e46bb4b02c33da253f901c473794e9-r0_arm64.deb "
+    dir=$(mktemp -d)
+
+    for _docker in ${docker};do
+        wget --directory-prefix ${dir} ${_docker}
+    done
+
+    dpkg -i ${dir}/*
+
+    rm -rf ${dir}
+
+}
+
+function rem_n_cleanup() {
+    apt-get purge docker-ce docker-ce-cli
+}
+
+rem_n_cleanup
+get_n_install
