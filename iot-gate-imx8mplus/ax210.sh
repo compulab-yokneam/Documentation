3ae36bfbfed66c92de7d2245554684a173319447
vraevsky <valentin@compulab.co.il>
2024-06-20 17:22:13 +0300

    Update ax210.sh

--- iot-gate-imx8mplus/ax210.sh
+++ iot-gate-imx8mplus/ax210.sh
@@ -3,7 +3,19 @@
 FW_DEB="https://github.com/compulab-yokneam/bin/raw/ax210/linux-firmware-ax210_20231030-r0_all.deb"
 FW_FILE=$(basename ${FW_DEB})
 
-rm -rf /tmp/${FW_FILE}
-wget --directory-prefix /tmp ${FW_DEB}
-dpkg -i /tmp/${FW_FILE}
-rm -rf /tmp/${FW_FILE}
+function fw_get() {
+	rm -rf /tmp/${FW_FILE}
+	wget --directory-prefix /tmp ${FW_DEB}
+	dpkg -i /tmp/${FW_FILE}
+	rm -rf /tmp/${FW_FILE}
+}
+
+function fw_sync() {
+	ind=($(stat --format=%i /lib/firmware /usr/lib/firmware | awk ''1 ORS=" "))
+	if [ ${ind[0]} -ne ${ind[1]} ];then
+		tar -C /usr/lib/firmware -cf - . | tar -C /lib/firmware -xf -
+	fi
+}
+
+fw_get
+fw_sync
