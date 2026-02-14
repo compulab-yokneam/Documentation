cc24bfce7aa99804ec7b005df71dea9a18432e70
vraevsky <valentin@compulab.co.il>
2026-02-14 18:09:26 +0200

    Refactor logs_status function to handle tmpfs

--- ucm-imx8m-plus/logs.sh
+++ ucm-imx8m-plus/logs.sh
@@ -9,17 +9,19 @@ function get_credentials() {
 }
 
 function logs_status() {
-	local logs_dev=$(findmnt /var/log -o SOURCE -n)
-	if [[ ${logs_dev:-"folder"} != "tmpfs" ]];then
-		echo "/var/log is a folder; most likely the logs already enabled"
-		return 1
+	local log_path="/var/log"                                                                                                                                                                                                                                                                                                                                                            │·········································································
+	local real_path=$(readlink -e ${log_path})                                                                                                                                                                                                                                                                                                                                           │·········································································
+	local logs_dev=$(findmnt ${log_path} -o SOURCE -n)
+	if [[ ${logs_dev:-"folder"} = "tmpfs" ]];then
+		sed -i 's/\(^.*\/var\/log\)/# \1/g' /etc/fstab
+		umount ${log_path} -l 2>/dev/null || true
 	fi
+	[[ ${real_path} = ${log_path} ]] && return 0 || true                                                                                                                                                                                                                                                                                                                                 │·········································································
+	unlink ${log_path}                                                                                                                                                                                                                                                                                                                                                                   │·········································································
 	return 0
 }
 
 function logs_enable() {
-	sed -i 's/\(^.*\/var\/log\)/# \1/g' /etc/fstab
-	umount /var/log -l 2>/dev/null || true
 	mkdir -p /var/log/journal || true
 	systemd-tmpfiles --create --prefix /var/log/journal || true
 cat << eof
