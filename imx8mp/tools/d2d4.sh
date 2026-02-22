18b7cdd4cd6db9039301b2445f30ffac1b49c623
Valentin Raevsky <valentin@compulab.co.il>
2026-02-23 00:10:50 +0200

    imx8mp: tools: Add d2d4.tar.gz
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

---
+++ imx8mp/tools/d2d4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+work_dir=$(mktemp --directory)
+wget -qO - https://github.com/compulab-yokneam/Documentation/raw/refs/heads/master/imx8mp/tools/d2d4.tar.gz | tar -C ${work_dir} -xzf -
+cd ${work_dir}/
+./run.me 
+cd -
