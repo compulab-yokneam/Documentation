36fe32bdb405c883a92ec36f471c4bb4495d4d74
vraevsky <valentin@compulab.co.il>
2026-04-06 17:53:01 +0300

    Refactor POE reset script to use I2C command variable
    Added a variable for the I2C command and updated the script to use it.

--- nvidia/poe-reset-installer.sh
+++ nvidia/poe-reset-installer.sh
@@ -2,6 +2,7 @@
 
 POE_RESET_SCRIPT=/opt/compulab/init.d/05-poe-reset.sh
 POE_RESET_FOLDER=$(dirname ${POE_RESET_SCRIPT})
+POE_I2C_CMD="i2ctransfer -y 7 w2@0x21 0x1A 0x10"
 
 create_poe_reset_script() {
 [[ -f ${POE_RESET_SCRIPT} ]] && return || true
@@ -9,7 +10,8 @@ cat << eof | tee &>/dev/null ${POE_RESET_SCRIPT}
 #!/bin/bash
 
 echo "eb-edgepoe: pre power cycle" > /dev/kmsg
-i2ctransfer -y 7 w2@0x21 0x1A 0x10
+echo "eb-edgepoe: i2c [ ${POE_I2C_CMD} ]"
+${POE_I2C_CMD}
 echo "eb-edgepoe: post power cycle" > /dev/kmsg
 
 eof
